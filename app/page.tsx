e4c10d664472bce48562ccac3cde1af4e48130d0
Amir Sorayaei <amir.s1377@gmail.com>
2023-08-26 00:57:13 +0330

    fix: updated page component with some global styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,19 @@
-import styles from "./page.module.scss";
+import Footer from "@/src/components/Footer";
+import Main from "@/src/content/Main";
+
 import "@/app/global.css";
+import Header from "@/src/components/Header";
+import Sidebar from "@/src/components/Siderbar";
 
 const Home = () => {
   return (
-    <main className={styles.main}>
-      <div>
-        <p>Hi, this is</p>
-        <h1>Amir Sorayaei</h1>
-        <h2>
-          Frontend Developer at&nbsp;
-          <a href="https://www.roomvu.com" target="blank">
-            Roomvu
-          </a>
-        </h2>
-      </div>
-    </main>
+    <div id="content">
+      <Header />
+      <Main />
+      <Sidebar orientation="left" />
+      <Sidebar orientation="right" />
+      <Footer />
+    </div>
   );
 };
 
