741ef7dfe42466ed6000a9955d48370ba4cffad2
Amir Sorayaei <amir.s1377@gmail.com>
2023-08-26 00:56:30 +0330

    feat: added footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Designed & Built by Amir Sorayaei</p>
+      <p style={{ marginTop: "10px" }}>2023</p>
+    </footer>
+  );
+};
+
+export default Footer;
