ae903792ec2b2b578146152a19610f6c3a135b13
amirsorayaei <35744145+amirsorayaei@users.noreply.github.com>
2023-08-26 13:32:00 +0330

    feat: added motion to parent div
    Also refactored folder structure

--- src/content/Main/index.tsx
+++ src/content/Main/index.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 
-import styles from "./index.module.scss";
+import About from "../About";
+import Title from "../Title";
 
 const Main = () => {
   return (
     <main>
-      <div className={styles.main}>
-        <p>Hi, this is</p>
-        <h1>Amir Sorayaei</h1>
-        <h2>
-          Frontend Developer at&nbsp;
-          <a href="https://www.roomvu.com" target="blank">
-            Roomvu
-          </a>
-        </h2>
-      </div>
+      <Title />
+      <About />
     </main>
   );
 };
