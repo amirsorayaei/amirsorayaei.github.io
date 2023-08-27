61e2384ce3eaa8129129fda6ca48821017a7ad98
amirsorayaei <35744145+amirsorayaei@users.noreply.github.com>
2023-08-27 15:00:01 +0330

    fix: refactored about with page component

--- src/content/About/index.tsx
+++ src/content/About/index.tsx
@@ -2,15 +2,16 @@ import React from "react";
 
 import FadeInWhenVisible from "@/src/components/FadeInWhenVisible";
 import styles from "./index.module.scss";
+import Page from "@/src/components/Page";
 
 const About = () => {
   return (
     <FadeInWhenVisible>
-      <div id="about" className={styles.wrapper}>
+      <Page id="about" className={styles.wrapper}>
         <p>
           {`An experienced React developer with 4+ years of industrial experience in developing and maintaining web applications and dashboard panels. I’ve worked on various platforms and have successfully delivered products with 50k active users. My expertise lies in creating user-friendly, responsive and interactive web applications using React JS. I’m also well-versed in debugging, troubleshooting and optimizing the performance of the applications.`}
         </p>
-      </div>
+      </Page>
     </FadeInWhenVisible>
   );
 };
