50478555fbc4918a7dfdc54739fc03a72f1ff007
amirsorayaei <35744145+amirsorayaei@users.noreply.github.com>
2023-08-27 11:11:49 +0330

    fix: changed transition duration

--- src/components/FadeInWhenVisible/index.tsx
+++ src/components/FadeInWhenVisible/index.tsx
@@ -14,10 +14,10 @@ const FadeInWhenVisible = ({ children, className }: Props) => {
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true }}
-      transition={{ duration: 0.7 }}
+      transition={{ duration: 0.5 }}
       variants={{
+        hidden: { opacity: 0, scale: 0.8 },
         visible: { opacity: 1, scale: 1 },
-        hidden: { opacity: 0, scale: 0.7 },
       }}
       className={className}
     >
