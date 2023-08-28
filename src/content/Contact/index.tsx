beabb7c3f52c9dd4042564f3429b8351c28d6f9b
amirsorayaei <35744145+amirsorayaei@users.noreply.github.com>
2023-08-28 12:39:10 +0330

    feat: added emailing feature to contact section

--- src/content/Contact/index.tsx
+++ src/content/Contact/index.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
+
+import Loading from "@/src/components/Loading";
+import { Result } from "@/src/types/types";
 
 import styles from "./index.module.scss";
 
@@ -8,9 +12,62 @@ const Contact = () => {
   const [fullname, setFullName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [message, setMessage] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [result, setResult] = useState<Partial<Result>>();
 
-  return (
-    <div id="contact" className={styles.wrapper}>
+  /**
+   * Function to send email to the site owner
+   */
+  const submit = async () => {
+    const data = {
+      from_name: fullname,
+      from_email: email,
+      message,
+    };
+
+    setLoading(true);
+    /**
+     * Send the message and get a callback with an error or details of the message that was sent
+     */
+    emailjs
+      .send("service_1scq7hk", "template_bnzlyfl", data, "beNkgA60nsq3eMwYk")
+      .then(
+        () => {
+          setResult({
+            message:
+              "Your message has been sent successfully!\nI will reach out to you ASAP.",
+            type: "success",
+          });
+        },
+        () => {
+          setResult({
+            message: "Error to sending message. :(",
+            type: "error",
+          });
+        }
+      )
+      .finally(() => setLoading(false));
+  };
+
+  const renderContent = () => {
+    /**
+     * Display success message
+     */
+    if (result?.type === "success") {
+      return <h2 style={{ color: "#4caf50" }}>{result.message}</h2>;
+    }
+
+    /**
+     * Display error message
+     */
+    if (result?.type === "error") {
+      return <h2 style={{ color: "#f44336" }}>{result.message}</h2>;
+    }
+
+    /**
+     * Form content
+     */
+    return (
       <div className={styles.form}>
         <h1>Contact Me</h1>
         <h4>Let me know what you need!</h4>
@@ -28,6 +85,7 @@ const Contact = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="ex: you@company.com"
+            type="email"
           />
         </div>
         <div className={styles.inputContainer}>
@@ -38,8 +96,16 @@ const Contact = () => {
             placeholder={`How can I help you?\nWrite it down here ...`}
           />
         </div>
-        <button>Submit</button>
+        <button disabled={loading} onClick={submit}>
+          {loading ? <Loading /> : "Submit"}
+        </button>
       </div>
+    );
+  };
+
+  return (
+    <div id="contact" className={styles.wrapper}>
+      {renderContent()}
     </div>
   );
 };
