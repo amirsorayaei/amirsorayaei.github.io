b108a747bd576aa8e253714145da33e1753c3a08
amirsorayaei <35744145+amirsorayaei@users.noreply.github.com>
2023-08-28 15:06:47 +0330

    feat: added root scss file with global variables

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Footer from "@/src/components/Footer";
 import Main from "@/src/content/Main";
 
 import "@/app/global.scss";
+import "@/app/root.scss";
+
 import Header from "@/src/components/Header";
 import Sidebar from "@/src/components/Siderbar";
 import Contact from "@/src/content/Contact";
